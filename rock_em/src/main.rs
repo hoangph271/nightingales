fdebf384d3bdf5f82ade2c796cdd1590c664d8cb
hoang.phanhuy <hoang.phanhuy@vietis.com.vn>
2020-11-20 18:55:10 +0700

    [💩] WIP - API with Sqlite

--- rock_em/src/main.rs
+++ rock_em/src/main.rs
@@ -4,10 +4,20 @@
 extern crate rocket;
 extern crate rocket_multipart_form_data;
 
+use byte_unit;
 use rocket_multipart_form_data as rmfd;
+use rocket_multipart_form_data::MultipartFormDataError;
 
 use rocket::http::ContentType;
 use rocket::{Data, Request};
+
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket_contrib::databases::diesel;
+
+#[database("sqlite_logs")]
+struct LogsDbConn(diesel::SqliteConnection);
 
 #[get("/")]
 fn index() -> &'static str {
@@ -20,24 +30,33 @@ enum ApiResponse {
     BadRequest(String),
     #[response(status = 404)]
     NotFound(String),
+    #[response(status = 413)]
+    RequestEntityTooLarge(String),
     #[response(status = 201)]
     Created(String),
 }
 
 #[post("/users", data = "<data>")]
-fn post_users(content_type: &ContentType, data: Data) -> ApiResponse {
-    // TODO: NOT unwrap...! :"<
+fn post_users(conn: LogsDbConn, content_type: &ContentType, data: Data) -> ApiResponse {
+    println!("{:?}", conn);
+
     let mut options = rmfd::MultipartFormDataOptions::with_multipart_form_data_fields(vec![
         match rmfd::MultipartFormDataField::file("avatar")
+            .size_limit(byte_unit::n_mb_bytes!(4) as u64)
             .content_type_by_string(Some(rmfd::mime::IMAGE_STAR))
         {
             Ok(avatar) => avatar,
             Err(e) => {
                 eprintln!("{:?}", e);
-                return ApiResponse::BadRequest(String::from("avatar field is invalid"));
+
+                // TODO: Split handling 412, 400...?
+                // match e {
+                // }
+
+                return ApiResponse::BadRequest(String::from("avatar field is not an image"));
             }
         },
-        // rmfd::MultipartFormDataField::raw("fingerprint").size_limit(4096),
+        rmfd::MultipartFormDataField::raw("fingerprint"),
         rmfd::MultipartFormDataField::text("email"),
         rmfd::MultipartFormDataField::text("password"),
         rmfd::MultipartFormDataField::text("fullname"),
@@ -59,7 +78,7 @@ fn post_users(content_type: &ContentType, data: Data) -> ApiResponse {
     println!("{:?}", avatar);
     let email = match form_data.texts.remove("email") {
         Some(email) => email,
-        None => return ApiResponse::BadRequest(String::from(("email is requried"))),
+        None => return ApiResponse::BadRequest(String::from("email is requried")),
     };
     let password = form_data.texts.remove("password");
     let fullname = form_data.texts.remove("fullname");
@@ -87,6 +106,7 @@ fn not_found(_req: &Request) -> &'static str {
 
 fn main() {
     rocket::ignite()
+        .attach(LogsDbConn::fairing())
         .register(catchers![not_found])
         .mount("/", routes![post_users])
         .mount("/", routes![index])
